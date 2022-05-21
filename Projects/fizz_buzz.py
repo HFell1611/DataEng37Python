a427be914961671032e0b785fd9c4355b484a948
ADisturbedSqrl <harry.fellows1996@gmail.com>
2022-05-21 14:55:30 +0100

    Added a 'while' solution

--- Projects/fizz_buzz.py
+++ Projects/fizz_buzz.py
@@ -1,15 +1,15 @@
 # Fizz Buzz
 
-# for x in range(1, 101):
-#     if x % 3 == 0 and x % 5 == 0:
-#         print("FizzBuzz")
-#     elif x % 3 == 0:
-#         print("Fizz")
-#     elif x % 5 == 0:
-#         print("Buzz")
-#     else:
-#         print(x)
-#
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+
 x = 1
 
 while x < 101:
