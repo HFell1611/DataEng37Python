874fadc86267fea4c2329cdcd8661949227383c1
ADisturbedSqrl <harry.fellows1996@gmail.com>
2022-05-23 17:08:15 +0100

    added option to pick number to run to

--- Projects/fizz_buzz.py
+++ Projects/fizz_buzz.py
@@ -1,30 +1,44 @@
 # Fizz Buzz
 
-run = input("Which program do you want to run?: for/while\n")
+check_input = True
+check_num = True
 
-if run.lower() == "for":
-    for x in range(1, 101):
-        if x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz")
-        elif x % 3 == 0:
-            print("Fizz")
-        elif x % 5 == 0:
-            print("Buzz")
-        else:
-            print(x)
+while check_input:
+    run = input("Which program do you want to run?: for/while\n")
+    if run.lower() == "for":
+        while check_num:
+            num = input("What number do you want to run to?\n")
+            if num.isdigit():
+                num_int = int(num)
+                for x in range(1, num_int + 1):
+                    if x % 3 == 0 and x % 5 == 0:
+                        print("FizzBuzz")
+                    elif x % 3 == 0:
+                        print("Fizz")
+                    elif x % 5 == 0:
+                        print("Buzz")
+                    else:
+                        print(x)
+            else:
+                print("Please enter a valid number")
 
-elif run.lower() == "while":
-    x = 1
-    while x < 101:
-        if x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz")
-        elif x % 3 == 0:
-            print("Fizz")
-        elif x % 5 == 0:
-            print("Buzz")
-        else:
-            print(x)
-        x += 1
-
-else:
-    print("Incorrect option")
+    elif run.lower() == "while":
+        while check_num:
+            num = input("What number do you want to run to?\n")
+            if num.isdigit():
+                num_int = int(num)
+                x = 1
+                while x < num_int + 1:
+                    if x % 3 == 0 and x % 5 == 0:
+                        print("FizzBuzz")
+                    elif x % 3 == 0:
+                        print("Fizz")
+                    elif x % 5 == 0:
+                        print("Buzz")
+                    else:
+                        print(x)
+                    x += 1
+            else:
+                print("Please enter a valid number")
+    else:
+        print("Please input either for or while.")
