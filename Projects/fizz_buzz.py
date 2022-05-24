3e7208f150600a5e87d3d4b9d2104cd604e21ced
ADisturbedSqrl <harry.fellows1996@gmail.com>
2022-05-24 09:26:23 +0100

    stopped program constantly running

--- Projects/fizz_buzz.py
+++ Projects/fizz_buzz.py
@@ -6,10 +6,12 @@ check_num = True
 while check_input:
     run = input("Which program do you want to run?: for/while\n")
     if run.lower() == "for":
+        check_input = False
         while check_num:
             num = input("What number do you want to run to?\n")
             if num.isdigit():
                 num_int = int(num)
+                check_num = False
                 for x in range(1, num_int + 1):
                     if x % 3 == 0 and x % 5 == 0:
                         print("FizzBuzz")
@@ -23,11 +25,13 @@ while check_input:
                 print("Please enter a valid number")
 
     elif run.lower() == "while":
+        check_input = False
         while check_num:
             num = input("What number do you want to run to?\n")
             if num.isdigit():
                 num_int = int(num)
                 x = 1
+                check_num = False
                 while x < num_int + 1:
                     if x % 3 == 0 and x % 5 == 0:
                         print("FizzBuzz")
