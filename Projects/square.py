59b7b944d88cf8a9f1ebb529adce221c3bbfebd0
ADisturbedSqrl <harry.fellows1996@gmail.com>
2022-05-25 16:02:29 +0100

    made changes to square

--- Projects/square.py
+++ Projects/square.py
@@ -1,13 +1,19 @@
-class Square:
-    def __init__(self, length):
-        self.length = length
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self):
+        super().__init__(
+            width=8,
+            height=8
+        )
 
     def get_area(self):
-        area = self.length ** 2
+        area = self.width ** 2
         return f"The area is {area}"
 
     def get_perimeter(self):
-        perimeter = 4 * self.length
+        perimeter = 4 * self.width
         return f"The perimeter is {perimeter}"
 
     def __repr__(self):
@@ -18,6 +24,7 @@ class Square:
 
 
 if __name__ == "__main__":
-    square = Square(5)
-    print(square)
+    square = Square()
     print(square.get_area())
+    print(square.get_perimeter())
+    print(square)
