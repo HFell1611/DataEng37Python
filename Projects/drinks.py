3b97912df767f3f62a64f3a9e7d95e8995a8056b
ADisturbedSqrl <harry.fellows1996@gmail.com>
2022-05-26 16:38:08 +0100

    added return messages to drinks

--- Projects/drinks.py
+++ Projects/drinks.py
@@ -3,10 +3,11 @@ def orders():
         drinks_list = drinks.read().split('\n')
         order = input("What would you like to order?\n")
         if order.lower() in drinks_list:
-            with open("drinks_orders.txt", "w") as drink_orders:
-                drink_orders.write(order)
+            with open("drinks_orders.txt", "a") as drink_orders:
+                drink_orders.write(f"{order}\n")
+            return f"Added {order} to orders"
         else:
             return "Drink not on menu"
 
 
-orders()
+print(orders())
